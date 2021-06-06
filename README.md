636090d6c44c7897c0fd577bfa5f30d990d40a3d
Rockelarre <leogrisesgmail@example.com>
2021-06-06 06:04:47 -0400

    dev_1

---
+++ README.md
@@ -0,0 +1,36 @@
+# Desafio-Lista-de-Animales
+
+### Módulo 3
+### Programación avanzada en Javascript
+
+## Unidad: 2. Herencia
+
+#### Contenidos Desarrollados:
+
+- Herencia
+
+- Cadena de Prototipos
+
+- Getters, Setters y HTML
+
+
+## Programa de Desarrollo de Aplicaciones FullStack JAVASCRIPT
+
+- Módulo 1: Fundamentos Desarrollo Web
+
+- Módulo 2: Programación básica en JavaScript
+
+- Módulo 3: Programación avanzada en Javascript
+
+- Módulo 4: Desarrollo de aplicaciones web Frontend
+
+- Módulo 5: Lenguaje de consultas a una base de datos PostgreSQL
+
+- Módulo 6: Desarrollo Backend en Node
+
+- Módulo 7: Acceso a base de datos con Node
+
+- Módulo 8: Framework Express
+
+
+### Edutecno - Desafío Latam - SENCE
---
+++ assets/js/script.js
@@ -0,0 +1,96 @@
+// Desafío - Lista de Animales
+// Autor: Daniel Vega - https://cianware.com
+
+// Definición de clase
+class Propietario{
+    constructor(nombre,direccion,telefono){
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+    }
+
+    // Método para generar el mensaje con la información del dueño de la mascota
+    datosPropietario(){
+        return `El nombre del dueño es: ${this.nombre}. El domicilio es ${this.direccion}, y el número telefónico de contacto: ${this.telefono}`;
+    }
+}
+
+// Definición de clase
+class Animal extends Propietario{
+    constructor(nombre,direccion,telefono,tipo){
+        super(nombre,direccion,telefono);
+        this._tipo = tipo;
+    }
+
+    // Método Get
+    get tipo(){
+        return this._tipo;
+    }
+}
+
+// Definición de clase
+class Mascota extends Animal{
+    constructor(nombre,direccion,telefono,tipo,nombreMascota,enfermedad){
+        super(nombre,direccion,telefono,tipo);
+        this._nombreMascota = nombreMascota;
+        this._enfermedad = enfermedad;
+    }
+
+    // Método Get
+    get nombreMascota(){
+        return this._nombreMascota;
+    }
+
+    // Método Set
+    set nombreMascota(nuevo_nombreMascota){
+        this._nombreMascota = nuevo_nombreMascota;
+    }
+
+    // Método Get
+    get enfermedad(){
+        return this._enfermedad;
+    }
+
+    // Método Set
+    set enfermedad(nuevo_enfermedad){
+        this._enfermedad = nuevo_enfermedad;
+    }
+}
+
+//----------------------------------------------------
+
+// Obteniendo formulario
+let formulario = document.querySelector('form');
+
+// Método para obtener los datos desde el formulario, desplegar información
+// en la página según la opción elegida en el elemento <select>, a través
+// de dos elementos <li> incrustados en el elemento <ul>
+let consulta = (event) => {
+    event.preventDefault();
+
+    let dueño = document.getElementById('propietario').value;
+    let telefono = document.getElementById('telefono').value;
+    let direccion = document.getElementById('direccion').value;
+    let nombreMascota = document.getElementById('nombreMascota').value;
+    let tipo = document.getElementById('tipo').value;
+    let enfermedad = document.getElementById('enfermedad').value;
+    let resultado = document.querySelector('ul');
+
+    if( tipo == 'perro'){
+        let perro = new Mascota(dueño,direccion,telefono,tipo,nombreMascota,enfermedad);
+        resultado.innerHTML = `<li>${perro.datosPropietario()}</li>
+                                <li>El tipo de animal es un: ${perro.tipo}, mientras que el nombre de la mascota es: ${perro.nombreMascota} y la enfermedad es: ${perro.enfermedad}</li>`;
+    } else if (tipo == 'gato'){
+        let gato = new Mascota(dueño,direccion,telefono,tipo,nombreMascota,enfermedad);
+        resultado.innerHTML = `<li>${gato.datosPropietario()}</li>
+                                <li>El tipo de animal es un: ${gato.tipo}, mientras que el nombre de la mascota es: ${gato.nombreMascota} y la enfermedad es: ${gato.enfermedad}</li>`;
+    } else if (tipo == 'conejo'){
+        let conejo = new Mascota(dueño,direccion,telefono,tipo,nombreMascota,enfermedad);
+        resultado.innerHTML = `<li>${conejo.datosPropietario()}</li>
+                                <li>El tipo de animal es un: ${conejo.tipo}, mientras que el nombre de la mascota es: ${conejo.nombreMascota} y la enfermedad es: ${conejo.enfermedad}</li>`;
+    }
+}
+
+// Método de escucha incluido en el formulario, que lanzará el método
+// consulta() al hacer click en el botón del formulario.
+formulario.addEventListener('submit',consulta);
